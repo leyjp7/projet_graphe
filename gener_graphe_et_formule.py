8fdd9324609bb9c4b89330e80106bcd038da2218
leyjp7 <52263412+leyjp7@users.noreply.github.com>
2021-04-26 00:19:39 +0200

    Update gener_graphe_et_formule.py

--- gener_graphe_et_formule.py
+++ gener_graphe_et_formule.py
@@ -1,5 +1,12 @@
 from random import *
+import math
+import turtle
 
+
+s = turtle.getscreen()
+t = turtle.Turtle()
+
+pi = math.pi
 
 # Programme qui cree un graphe G quelconque a n sommets pour n entre 0 et 10.
 
@@ -7,7 +14,8 @@ from random import *
 # M[i][j] = 1 signifie que les sommets i et j sont relies par un arete
 # et M[i][j] = 0 singifie qu'il n'y a pas d'arete entre ces deux sommets.
 
-n = randint(0, 5)
+n = randint(0, 10)
+
 
 def generer_graphe(n):
   g = []
@@ -31,6 +39,11 @@ def generer_graphe(n):
 #      b = randint(0, 1)
 #      g[i][j] = b
 #  return g
+
+
+def coords_sur_circum(r,n):
+    return [(math.cos(2*pi/n*x)*r,math.sin(2*pi/n*x)*r) for x in range(0,n)]
+    
 
 def trouver_ens_aretes(G, n):
     ens = []
@@ -60,21 +73,71 @@ def formule_depuis_graphe(G, n, C):
                 clause2 = [(-l, j+1), (-k, j+1)]
                 fnc.append(clause2)
     return fnc
-                
 
+def remplir_dic(A, coords, dic):
+  
+    for (l, k) in A:
+        dic[l][0].append(k)
+        dic[k][0].append(l)
+        
+    for i in range(n):
+        dic[i+1][1] = coords[i]
+    return dic
+        
 
+dic = { i+1 : [[],0] for i in range(n) }
 
-print(n)
+coords = coords_sur_circum(70, n)
+
+def dessine_sommets(dic, n):
+      r = 70 / n
+      t.penup()
+      for i in range(n):
+          (x, y) = dic[i+1][1]
+          t.goto(x, y)
+          t.pendown()
+          t.circle(r)
+          t.penup()
+
+def dessine_aretes(dic, n):
+      t.penup()
+      for i in range(n):
+        
+          (x1, y1) = dic[i+1][1]
+           
+          for j in dic[i+1][0]:
+              (x2, y2) = dic[j][1]
+              t.goto(x1, y1)
+              t.pendown()
+              t.goto(x2, y2)
+              t.penup()
+
+def dessine_graphe(dic, n):
+    dessine_sommets(dic, n)
+    dessine_aretes(dic, n)
+              
+    
+print(coords)
+
+    
+
+print('n : ', n)
 G = generer_graphe(n)
-print(G)
+print('matrice d adjacents : ', G)
 print('\n')
-aretes = trouver_ens_aretes(G, n)
-print(aretes)
+A = trouver_ens_aretes(G, n)
+print('ens d aretes : ', A)
+d = remplir_dic(A, coords, dic)
+print('dico ',d)
+print('\n')
+dessine_graphe(d, n)
+print('\n')
+print('\n')
 print('\n')
 C = gen_ens_couleurs(n)
-print(C)
+print('ens de couleurs : ',C)
 print('\n')
 formule = formule_depuis_graphe(G, n, C)
-print(formule)
+print('formule en fnc representee : ',formule)
 #G = definition_graphe(n, g)
 #print(G)
